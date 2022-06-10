1046f14736d01ceec6b5fdd5e3c41b87e8f52806
zrlange <58605629+zrlange@users.noreply.github.com>
2022-06-10 10:13:01 -0500

    back to Rocket  - troubleshooting connection

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,11 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+#[macro_use] extern crate rocket;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+#[get("/hello/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+  format!("Hello, {} year old named {}!", age, name)
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("0.0.0.0", 80))?
-    .run()
-    .await
+#[launch]
+fn rocket() -> _ {
+  rocket::build().mount("/", routes![hello])
 }
