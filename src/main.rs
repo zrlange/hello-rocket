d84e5e6aee36e26014dfa051bbe496d5aff4f298
zrlange <58605629+zrlange@users.noreply.github.com>
2022-06-10 08:35:24 -0500

    corrected about 404 issue

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
-  "Hello, world!"
-}
-
-#[get("/about")]
-fn about() -> &'static str {
-  "This is the about page of my Rocket Demo app"
+    "Hello, world!"
 }
 
 #[launch]
 fn rocket() -> _ {
-  //rocket::build().mount("/", routes![index])
-  rocket::build().mount("/about", routes![index])
+    rocket::build().mount("/", routes![index])
 }
-
